53268e7fd053b258b6e6a2061968bc7eeef0cc51
Nimrod Shanni <nimrodshanni@campus.technion.ac.il>
2023-02-27 20:22:49 +0200

    finished full basic functionality . creating GUI

--- courseRegistrationHelper.py
+++ courseRegistrationHelper.py
@@ -12,9 +12,8 @@ TIMEOUT = 10
 REGISTRATION_TIMEOUT = 30
 IS_AVAILABLE = "text-success"
 
-driver = None
 
-def login() -> None:
+def login() -> webdriver:
     driver = webdriver.Chrome(PATH)
     driver.get("https://students.technion.ac.il/auth/oidc/")
     driver.maximize_window()
@@ -27,13 +26,14 @@ def login() -> None:
         WebDriverWait(driver, TIMEOUT).until(EC.element_to_be_clickable((By.ID, "idSIButton9"))).click()
     except:
         driver.quit()
+    return driver
 
 
-def get_to_course_page(course_number:str) -> None:
+def get_to_course_page(driver:webdriver, course_number:str) -> None:
     driver.get("https://students.technion.ac.il/local/technionsearch/course/" + course_number)
 
 
-def get_spans_xpath():
+def get_spans_xpath(driver:webdriver):
     SPANS_XPATH = "/html/body/div[2]/div[3]/div/div/section/div/div/div[3]/div/div[1]/div/span"
     groups = driver.find_elements(By.XPATH, SPANS_XPATH)
     if len(groups) == 0:
@@ -42,10 +42,10 @@ def get_spans_xpath():
     return groups, SPANS_XPATH
 
 
-def is_group_available(course_number:str, group_number:str) -> bool:
+def is_group_available(driver:webdriver, course_number:str, group_number:str) -> bool:
 
-    get_to_course_page(course_number)
-    groups, SPANS_XPATH = get_spans_xpath()
+    get_to_course_page(driver, course_number)
+    groups, SPANS_XPATH = get_spans_xpath(driver)
     result = False
     for index in range(len(groups)):
         if driver.find_element(By.XPATH, SPANS_XPATH + "[" + str(index+1) + "]/div/table/tbody/tr/td[1]/table/tbody/tr[2]/td/div").get_attribute("data-group_id") == group_number and driver.find_element(By.XPATH, SPANS_XPATH + "[" + str(index+1) + "]/div/table/tbody/tr/td[1]/table/tbody/tr[1]/td[2]/div[2]").get_attribute("class") == IS_AVAILABLE:
@@ -53,23 +53,50 @@ def is_group_available(course_number:str, group_number:str) -> bool:
     return result
 
 
-def checkout_cart() -> None:
+def checkout_cart(driver:webdriver) -> None:
     driver.get("https://students.technion.ac.il/local/tregister/cart")
     WebDriverWait(driver, TIMEOUT).until(EC.element_to_be_clickable((By.ID, "process_cart_item_request"))).click()
     time.sleep(REGISTRATION_TIMEOUT)
 
 
-def register(course_list:list, single_course_registration:bool) -> None:
+def register(driver:webdriver, course_list:list, single_course_registration:bool) -> None:
     try:
         while True:
             for course in course_list:
                 course_elements = course.split("-")
-                if is_group_available(course_elements[0], course_elements[1]):
-                    checkout_cart()
+                if is_group_available(driver, course_elements[0], course_elements[1]):
+                    checkout_cart(driver)
                     if single_course_registration:
                         raise KeyboardInterrupt
                     else:
                         course_list.remove(course)
                         break
+        time.sleep(15)
     except KeyboardInterrupt:
         pass
+
+# # version with printing instead of checking out
+# def register(driver:webdriver, course_list:list, single_course_registration:bool) -> None:
+#     try:
+#         while True:
+#             for course in course_list:
+#                 course_elements = course.split("-")
+#                 if is_group_available(driver, course_elements[0], course_elements[1]):
+#                     #checkout_cart(driver)
+#                     print("registered: " + course)
+#                     if single_course_registration:
+#                         raise KeyboardInterrupt
+#                     else:
+#                         course_list.remove(course)
+#                         break
+#                 else:
+#                     print("no room at: " + course)
+#             time.sleep(15)
+#     except KeyboardInterrupt:
+#         pass
+
+
+def handler(course_list:list, single_course_registration:bool) -> None:
+    driver = login()
+    register(driver, course_list, single_course_registration)
+    driver.quit()
